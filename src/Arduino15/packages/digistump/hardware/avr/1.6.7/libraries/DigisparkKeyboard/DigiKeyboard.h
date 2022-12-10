61819e4e734ec63b7c55ab9b13eb6e24256c2bf6
mariusz <daniszewski@gmail.com>
2022-12-10 20:57:07 +0100

    added DigiKeyboard library modification

---
+++ src/Arduino15/packages/digistump/hardware/avr/1.6.7/libraries/DigisparkKeyboard/DigiKeyboard.h
@@ -0,0 +1,242 @@
+/*
+ * Based on Obdev's AVRUSB code and under the same license.
+ *
+ * TODO: Make a proper file header. :-)
+ * Modified for Digispark by Digistump
+ * Added full Keyboard usage values  by Danjovic, February 2016
+ * Report Buffer extended up to 6 keytrokes simultaneous by Danjovic, March 2016  
+ * Added LED control by Danjovic, January 2019
+ * Added poor event handling for LED state changes by MARD, December 2022
+ */
+#ifndef __DigiKeyboard_h__
+#define __DigiKeyboard_h__
+
+#include <Arduino.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include <avr/delay.h>
+#include <string.h>
+
+#include "usbdrv.h"
+#include "hidkeys.h"
+#include "scancode-ascii-table.h"
+
+// TODO: Work around Arduino 12 issues better.
+//#include <WConstants.h>
+//#undef int()
+
+typedef uint8_t byte;
+
+typedef struct {
+	uint8_t modifier;
+	uint8_t reserved;
+	uint8_t keycode[6];
+} keyboard_report_t;
+
+// static keyboard_report_t keyboard_report; // sent to PC
+ static volatile uchar ledState = 0xff; // received from PC
+ static uchar idleRate; // repeat rate for keyboards in 4 ms units
+
+
+#define NUM_LOCK (1<<0)
+#define CAPS_LOCK (1<<1)
+#define SCROLL_LOCK (1<<2)
+
+ 
+ // From Frank Zhao's USB Business Card project
+// http://www.frank-zhao.com/cache/usbbusinesscard_details.php
+const PROGMEM char usbHidReportDescriptor[USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH] = {
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x06,                    // USAGE (Keyboard)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x95, 0x08,                    //   REPORT_COUNT (8)
+    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)(Key Codes)
+    0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)(224)
+    0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)(231)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs) ; Modifier byte
+    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x81, 0x03,                    //   INPUT (Cnst,Var,Abs) ; Reserved byte
+    0x95, 0x05,                    //   REPORT_COUNT (5)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x05, 0x08,                    //   USAGE_PAGE (LEDs)
+    0x19, 0x01,                    //   USAGE_MINIMUM (Num Lock)
+    0x29, 0x05,                    //   USAGE_MAXIMUM (Kana)
+    0x91, 0x02,                    //   OUTPUT (Data,Var,Abs) ; LED report
+    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x75, 0x03,                    //   REPORT_SIZE (3)
+    0x91, 0x03,                    //   OUTPUT (Cnst,Var,Abs) ; LED report padding
+    0x95, 0x06,                    //   REPORT_COUNT (6)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
+    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)(Key Codes)
+    0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))(0)
+    0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)(101)
+    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
+    0xc0                           // END_COLLECTION
+};
+ 
+
+
+class DigiKeyboardDevice : public Print {
+ public:
+  DigiKeyboardDevice () {
+    cli();
+    usbDeviceDisconnect();
+    _delay_ms(250);
+    usbDeviceConnect();
+
+
+    usbInit();
+
+    sei();
+
+    // TODO: Remove the next two lines once we fix
+    //       missing first keystroke bug properly.
+	memset((void *)&keyboard_report,0,sizeof(keyboard_report));
+	usbSetInterrupt((unsigned char*)&keyboard_report, sizeof(keyboard_report)); 
+  }
+  
+  
+  void update() {
+    usbPoll();
+  }
+
+	// delay while updating until we are finished delaying
+	void delay(long milli) {
+		unsigned long last = millis();
+	  while (milli > 0) {
+	    unsigned long now = millis();
+	    milli -= now - last;
+	    last = now;
+	    update();
+	  }
+	}
+
+
+   // get LED State
+   uchar getLEDs (void) {
+	   return ledState;
+   }
+
+
+  //sendKeyStroke: sends a key press AND release
+  void sendKeyStroke(byte keyStroke) {
+    sendKeyStroke(keyStroke, 0);
+  }
+
+  //sendKeyStroke: sends a key press AND release with modifiers
+  void sendKeyStroke(byte keyStroke, byte modifiers) {
+	sendKeyPress(keyStroke, modifiers);
+    // This stops endlessly repeating keystrokes:
+	sendKeyPress(0,0);
+  }
+
+  //sendKeyPress: sends a key press only - no release
+  //to release the key, send again with keyPress=0
+  void sendKeyPress(byte keyPress) {
+	sendKeyPress(keyPress, 0);
+  }
+
+  //sendKeyPress: sends a key press only, with modifiers - no release
+  //to release the key, send again with keyPress=0
+  void sendKeyPress(byte keyPress, byte modifiers) {
+   	while (!usbInterruptIsReady()) {
+      // Note: We wait until we can send keyPress
+      //       so we know the previous keyPress was
+      //       sent.
+    	usbPoll();
+    	_delay_ms(5);
+    }
+
+
+	memset((void *)&keyboard_report,0,sizeof(keyboard_report)); // memset(reportBuffer, 0, sizeof(reportBuffer));
+	keyboard_report.modifier = modifiers;              // reportBuffer[0] = modifiers; 
+	keyboard_report.keycode[1] = keyPress;             // reportBuffer[1] = keyPress; 
+
+	usbSetInterrupt((unsigned char*)&keyboard_report, sizeof(keyboard_report)); // usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
+   
+  }
+
+  size_t write(uint8_t chr) {
+    uint8_t data = pgm_read_byte_near(ascii_to_scan_code_table + (chr - 8));
+    sendKeyStroke(data & 0b01111111, data >> 7 ? MOD_SHIFT_RIGHT : 0);
+    return 1;
+  }
+
+  //private: TODO: Make friend?
+ // maximum 6 keystrokes, defined in HID report
+
+  keyboard_report_t keyboard_report; //  buffer for HID reports [ 1 modifier byte + (len-1) key strokes]
+  using Print::write;
+};
+
+DigiKeyboardDevice DigiKeyboard = DigiKeyboardDevice();
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+  // USB_PUBLIC uchar usbFunctionSetup
+	uchar usbFunctionSetup(uchar data[8]) {
+    usbRequest_t    *rq = (usbRequest_t *)((void *)data);
+
+//	usbMsgPtr = (unsigned char*)&DigiKeyboard.keyboard_report ;//usbMsgPtr = DigiKeyboard.reportBuffer; //
+	
+
+    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) {
+        switch(rq->bRequest) {
+        case USBRQ_HID_GET_REPORT: // send "no keys pressed" if asked here
+            // wValue: ReportType (highbyte), ReportID (lowbyte)
+			usbMsgPtr = (unsigned char*)&DigiKeyboard.keyboard_report ;//usbMsgPtr = DigiKeyboard.reportBuffer; //
+            DigiKeyboard.keyboard_report.modifier = 0;
+            DigiKeyboard.keyboard_report.keycode[0] = 0;
+            return sizeof(DigiKeyboard.keyboard_report);
+		case USBRQ_HID_SET_REPORT: // if wLength == 1, should be LED state
+            return (rq->wLength.word == 1) ? USB_NO_MSG : 0;
+        case USBRQ_HID_GET_IDLE: // send idle rate to PC as required by spec
+            usbMsgPtr = &idleRate;
+            return 1;
+        case USBRQ_HID_SET_IDLE: // save idle rate as required by spec
+            idleRate = rq->wValue.bytes[1];
+            return 0;
+        }
+    }
+    
+    return 0;
+  }
+ #ifdef __cplusplus
+} // extern "C"
+#endif
+ 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+// Poor implementation of event handler functionality. Takes 2 parameters: previous state and new state
+void (*onKeyboardLedChanged)(uint8_t, uint8_t) = NULL;
+
+// update LED state
+usbMsgLen_t usbFunctionWrite(uint8_t * data, uchar len) {
+	  
+    if (data[0] == ledState)
+        return 1;
+    else {
+		if(onKeyboardLedChanged) onKeyboardLedChanged(ledState, data[0]);
+        ledState = data[0];
+	}
+
+	return 1; // Data read, not expecting more	
+}
+  
+  
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+
+#endif // __DigiKeyboard_h__
